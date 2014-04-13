0f50c2eac9881b88bb0220aadbcca87346f68b94
Félix Hartmann <felix.hartmann@crans.org>
2014-04-13 12:54:18 +0200

    First commit.

---
+++ mpl_figure_editor.py
@@ -0,0 +1,74 @@
+
+from traits.etsconfig.api import ETSConfig
+ETSConfig.toolkit = 'qt4'
+
+import matplotlib as mpl
+mpl.rcParams['backend.qt4']='PySide'
+
+# We want matplotlib to use a Qt backend
+mpl.use('Qt4Agg')
+
+from pyface.qt import QtGui, QtCore
+from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_qt4agg import NavigationToolbar2QTAgg
+
+from traits.api import Any, Instance
+from traitsui.qt4.editor import Editor
+from traitsui.qt4.basic_editor_factory import BasicEditorFactory
+
+class _MPLFigureEditor(Editor):
+
+   scrollable  = True
+
+   def init(self, parent):
+       self.control = self._create_canvas(parent)
+       self.set_tooltip()
+
+   def update_editor(self):
+       pass
+
+   def _create_canvas(self, parent):
+       """ Create the MPL canvas. """
+       # matplotlib commands to create a canvas
+       frame = QtGui.QWidget()
+       mpl_canvas = FigureCanvas(self.value)
+       mpl_canvas.setParent(frame)
+       mpl_toolbar = NavigationToolbar2QTAgg(mpl_canvas,frame)
+
+       vbox = QtGui.QVBoxLayout()
+       vbox.addWidget(mpl_canvas)
+       vbox.addWidget(mpl_toolbar)
+       frame.setLayout(vbox)
+
+       return frame
+
+class MPLFigureEditor(BasicEditorFactory):
+
+   klass = _MPLFigureEditor
+
+
+if __name__ == "__main__":
+    # Create a window to demo the editor
+    from traits.api import HasTraits
+    from traitsui.api import View, Item
+    from numpy import sin, cos, linspace, pi
+
+    class Test(HasTraits):
+
+        figure = Instance(Figure, ())
+
+        view = View(Item('figure', editor=MPLFigureEditor(),
+                                show_label=False),
+                        width=400,
+                        height=300,
+                        resizable=True)
+
+        def __init__(self):
+            super(Test, self).__init__()
+            axes = self.figure.add_subplot(111)
+            t = linspace(0, 2*pi, 200)
+            axes.plot(sin(t)*(1+0.5*cos(11*t)), cos(t)*(1+0.5*cos(11*t)))
+
+    Test().configure_traits()
+
